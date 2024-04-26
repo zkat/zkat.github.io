20aea6fd1beae871a0987482410a295f4801d854
Kat Marchán <kzm@zkat.tech>
2024-04-26 09:17:53 -0700

    more prettier stuff!

--- importers/stargazer.ts
+++ importers/stargazer.ts
@@ -46,36 +46,71 @@ async function cleanupJournalEntry(entry: IJournalEntry): Promise<void> {
   const document = newDom.window.document;
   const newBody = newDom.window.document.body;
   let currentAction = null;
+  let figure = null;
   for (const el of dom.window.document.body.childNodes) {
     if (el instanceof dom.window.HTMLImageElement) {
+      figure = document.createElement("figure");
       const img = document.createElement("img");
       img.src = el.src;
-      newBody.appendChild(img);
+      figure.appendChild(img);
+      newBody.appendChild(figure);
       continue;
     }
-    const text = el.textContent.replaceAll("&nbsb;", "").trim();
-    if (text) {
-      if (text.startsWith("[")) {
+    const text = el.textContent
+      .replaceAll(/(&nbsb;|\n|\r|<br>|<\/br>)/gm, "")
+      .trim();
+    if (text && figure && text.startsWith("((")) {
+      const caption = document.createElement("figcaption");
+      caption.innerHTML = text
+        .replace(
+          /^\(\((?:Credit:\s*)?([^)]+?)\s*\)\)/i,
+          '<a target="_blank" href="$1">$1</a>'
+        )
+        .trim();
+      figure.appendChild(caption);
+      figure = null;
+    } else if (text) {
+      if (text.startsWith("———")) {
+        const hr = document.createElement("hr");
+        newBody.appendChild(hr);
+      } else if (text.startsWith("[")) {
         if (!currentAction) {
           currentAction = document.createElement("div");
           currentAction.classList.add("action");
           newBody.appendChild(currentAction);
         }
-        const actionItem = document.createElement("div");
-        actionItem.textContent = text.replace(/^\[([^\]]+)\]/, "$1").trim();
-        currentAction.appendChild(actionItem);
+        currentAction.appendChild(
+          makeActionItem(document, text.replace(/^\[([^\]]+)\]/, "$1").trim())
+        );
       } else {
         if (currentAction) {
           currentAction = null;
         }
         const note = document.createElement("p");
         note.innerHTML =
-          el instanceof dom.window.HTMLElement
-            ? el.innerHTML
-            : el.textContent;
+          el instanceof dom.window.HTMLElement ? el.innerHTML : el.textContent;
         newBody.appendChild(note);
       }
     }
   }
-  entry.content = newBody.innerHTML
+  entry.content = newBody.innerHTML;
+}
+
+function makeActionItem(
+  document: Document,
+  actionText: string
+): HTMLDivElement {
+  const actionItem = document.createElement("div");
+  if (actionText.startsWith("Miss")) {
+    actionItem.classList.add("roll", "miss");
+  } else if (actionText.startsWith("Weak Hit")) {
+    actionItem.classList.add("roll", "weak-hit");
+  } else if (actionText.startsWith("Strong Hit")) {
+    actionItem.classList.add("roll", "strong-hit");
+  }
+  if (actionText.toLowerCase().match("with a match")) {
+    actionItem.classList.add("match");
+  }
+  actionItem.textContent = actionText;
+  return actionItem;
 }
